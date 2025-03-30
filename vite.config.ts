a42f2b226ef2e17775c08971ce23ed6080c6dfe5
Noe Svanda <svanda.noe@gmail.com>
2025-03-31 01:09:01 +0200

    vite config adjustment + pipeline concurrency

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,23 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name || 'asset';
+          const extType = name.split('.').at(1) || 'unknown';
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return 'assets/img/[name]-[hash][extname]';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       }
     }
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+    host: true
   }
 })
